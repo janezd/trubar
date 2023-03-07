1a6e6ee1c27a99c4ca50e9acb8f31bf477ee1d49
janezd <janez.demsar@fri.uni-lj.si>
2023-03-07 20:18:10 +0100

    Merge: Fix unused reporting and tests

--- trubar/tests/shell_tests/merge/test.sh
+++ trubar/tests/shell_tests/merge/test.sh
@@ -27,16 +27,23 @@ print_run 'trubar merge faulty_translations.yaml tmp/translations-copy.yaml -o t
 diff tmp/translations-copy.yaml translations.yaml
 diff tmp/updated_translations.yaml exp/updated_translations_faulty.yaml
 diff tmp/unused.yaml exp/unused.yaml
-diff tmp/errors.txt exp/errors.txt
+if [[ ! -z $(cat tmp/errors.txt) ]]
+then
+    echo "merge mustn't output unused items when writing them to a file"
+    cat tmp/errors.txt
+    exit 1
+fi
 rm tmp/updated_translations.yaml tmp/unused.yaml tmp/errors.txt
 
 echo "... dry-run, with errors"
 print_run 'trubar merge faulty_translations.yaml tmp/translations-copy.yaml -u tmp/unused.yaml -n' tmp/errors.txt
 diff tmp/translations-copy.yaml translations.yaml
 diff tmp/unused.yaml exp/unused.yaml
-if [[ ! -z $(cat errors.txt) ]]
+if [[ ! -z $(cat tmp/errors.txt) ]]
 then
     echo "merge mustn't output unused items when writing them to a file"
+    cat tmp/errors.txt
+    exit 1
 fi
 rm tmp/unused.yaml tmp/errors.txt
 
